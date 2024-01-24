816077a968910a2fbe56db38f0a558ea6aea56b4
Monthira19 <poo19052545@hotmail.com>
2024-01-24 10:56:55 +0700

    เสร็จละ งิงิ

---
+++ src/components/form/text/Text.jsx
@@ -0,0 +1,394 @@
+import React from 'react'
+
+export default function Text() {
+  return (
+    <>
+        <div>
+      <div style={{ marginBottom: '20px' }}>
+        <img
+          src="./public/image/Auntie Man.webp"
+          alt="Auntie Man"
+          style={{ maxWidth: '80%', height: 'auto', marginBottom: '5px' }}
+        />
+      </div>
+      <div style={{ marginBottom: '20px' }}>
+        <img
+          src="./public/image/That was a brutal time.webp"
+          alt="That was a brutal time"
+          style={{ maxWidth: '80%', height: 'auto' }}
+        />
+      </div>
+      <div style={{ fontSize: '40px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px', 
+      marginTop: '20px', marginLeft: '120px' }}>
+        อุ๊ย! ตีนกาขึ้นใช่ไหม
+      </div>
+
+
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="/public/image/cross.webp"
+            alt="cross"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px', marginLeft: '120px' ,marginRight: '120px' }}>
+          อายุที่มากขึ้น ทำให้ร่างกายผลิตคอลลาเจนน้อยลงผิวจึงจะหย่อนคล้อยและเกิดริ้วรอยได้ง่าย ซึ่งบริเวณหางตามักเป็นริ้วรอยเป็นลำดับต้นๆ 
+          หรือการยิ้ม การหัวเราะ การร้องไห้ การหรี่ตา ล้วนเป็นการขยับกล้ามเนื้อบริเวณหางตา ซึ่งการขยับกล้ามเนื้อซ้ำๆ เป็นประจำ 
+          อาจทำให้กล้ามเนื้อบริเวณดังกล่าวหย่อนคล้อยและเกิดริ้วรอยได้
+        </div>
+      </div>
+      <div style={{ fontSize: '40px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px',marginLeft: '120px' }}>
+      เห้อ! มีริ้วรอยใช่ไหม
+      </div>
+      <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Wrinkles, yes.webp"
+            alt="Wrinkles, yes"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        รังสียูวีในแสงแดดจะทำลายคอลลาเจน ส่งผลให้เกิดริ้วรอย หรือ การรับประทานอาหารที่ขาดสารอาหาร เช่น วิตามินซี วิตามินอี 
+        และกรดไขมันโอเมก้า 3 ที่มีส่วนช่วยในการเสริมสร้างคอลลาเจนและอิลาสตินในผิว หากได้รับไม่เพียงพออาจทำให้เกิดริ้วรอยมากขึ้นได้
+
+
+        </div>
+      <div style={{ fontSize: '40px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px',marginLeft: '120px' }}>
+      เอ๊ะ! ผิวหมองคล้ำใช่ไหม
+      </div>
+      <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Dull skin.webp"
+            alt="Dull skin"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        อายุมากขึ้นเมื่อ ร่างกายจะผลิตเมลานินลดลง ทำให้ผิวดูหมองคล้ำลง และโรคบางชนิดอาจส่งผลต่อการทำงานของระบบเม็ดสีในผิว ทำให้ผิวหมองคล้ำ
+
+
+
+        </div>
+      <div style={{ fontSize: '40px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px',marginLeft: '120px' }}>
+      ห๊ะ! ผิวกำลังเหี่ยวย่น
+      </div>
+      <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/wrinkled skin.webp"
+            alt="wrinkled skin"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        เกิดจากร่างกายผลิตคอลลาเจนและอิลาสตินลดลงซึ่งเป็นโปรตีนที่ช่วยให้ผิวแข็งแรงและยืดหยุ่น อายุมากขึ้นโปรตีนเหล่านี้ก็จะน้อยลง
+
+        </div>
+      <div style={{ fontSize: '40px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px',marginLeft: '120px' }}>
+      โห! พุงยื่นออกมาใช่ไหม
+      </div>
+      <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Protruding belly.webp"
+            alt="Protruding belly"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        น้ำหนักตัวเพิ่มขึ้น ไขมันจะสะสมในบริเวณต่างๆ ของร่างกาย รวมถึงบริเวณหน้าท้องซึ่งไขมันที่สะสมบริเวณหน้าท้องทำให้หน้าท้องยื่นออกมา
+
+        </div>
+      <div style={{ fontSize: '40px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px',marginLeft: '120px' }}>
+      โอ๊ย! ปวดข้อปวดเข่าอยู่รึป่าว
+      </div>
+      <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Knee pain.webp"
+            alt="Knee pain"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        อายุมากขึ้น ร่างกายจะเสื่อมสภาพลงทำให้กระดูกอ่อนบริเวณข้อ เสื่อมสภาพส่งผลให้กระดูกเสียดสีกันได้ง่าย ทำให้เกิดอาการปวดข้อ
+
+        
+        </div>
+      <div style={{ fontSize: '40px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px',marginLeft: '120px' }}>
+      เห้อ! เนื้อหนังหย่อนยานใช่ไหม
+      </div>
+      <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Yan skin.webp"
+            alt="Yan skin"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        อายุที่มากขึ้น ร่างกายจะผลิตคอลลาเจนและอิลาสตินได้น้อยลง จึงทำให้ผิวหย่อนคล้อย
+        <div style={{ fontSize: '50px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px', color: 'red' }}>รู้สึกไม่พอใจ <span style={{ fontSize: '30px', color: 'black' }}>ขาดความมั่นใจในผิวพรรณ และรูปร่างของตัวเอง</span></div>
+    </div>
+    <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/notsatisfied.jpg"
+            alt="Dress nicely"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        อายุที่มากขึ้น ร่างกายจะผลิตคอลลาเจนและอิลาสตินได้น้อยลง จึงทำให้ผิวหย่อนคล้อย
+        
+
+        
+        <div style={{ fontSize: '50px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px', color: 'red' }}>อยากแต่งตัวสวยๆ <span style={{ fontSize: '30px', color: 'black' }}>เหมือนกับคนอื่นแต่ผิวกับไม่มีออร่า</span></div>
+    </div>
+    <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Dress nicely.jpg"
+            alt="Dress nicely"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        <div style={{ fontSize: '50px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px', color: 'red' }}>
+            ระบบเผาผลาญ  <span style={{ fontSize: '30px', color: 'black' }}>ทำงานไม่มีประสิทธิภาพ กินนิดกินหน่อยก็อ้วน</span></div>
+    </div>
+    <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Metabolic system.jpg"
+            alt="Metabolic system"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        <div style={{ fontSize: '50px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px', color: 'red' }}>
+        อายุมากขึ้น  <span style={{ fontSize: '30px', color: 'black' }}>ทำงานไม่มีประสิทธิภาพ กินนิดกินหน่อยก็อ้วน</span></div>
+    </div>
+    <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Gettingolder.jpg"
+            alt="Gettingolder"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        <div style={{ fontSize: '50px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px', color: 'red' }}>
+        ไม่มีเวลา  <span style={{ fontSize: '30px', color: 'black' }}>ในการดูแลตัวเองทั้งการบำรุงผิว และการออกกำลังกาย</span></div>
+    </div>
+    <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/notime.jpg"
+            alt="notime"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        <div style={{ fontSize: '50px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px', color: 'red' }}>
+        อยากควบคุมการกิน  <span style={{ fontSize: '30px', color: 'black' }}>แต่เห็นของน่ากินแล้วก็หยุดซื้อไม่ได้</span></div>
+    </div>
+    <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/controleating.jpg"
+            alt="controleating"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px',marginLeft: '120px', marginRight: '100px' }}>
+        <div style={{ fontSize: '50px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '20px', color: 'red' }}>
+        ลองบำรุงผิว  <span style={{ fontSize: '30px', color: 'black' }}>มาหลายวิธี ทั้งกิน ทั้งทา ก็เกิดผลลัพธ์แต่เสี่ยงอันตราย</span></div>
+    </div>
+    <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/Skincare.jpg"
+            alt="Skincare"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', marginBottom: '10px', textAlign: 'center' }}>
+  <div style={{ fontSize: '50px', fontFamily: 'Kanit', marginBottom: '20px', color: 'black' }}>
+    หมดเงิน หลายหมื่น แถม <span style={{ fontSize: '30px', color: 'red' }}>ไม่เห็นผล</span>
+  </div>
+</div>
+
+<div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px', marginLeft: '120px' ,marginRight: '120px' }}>
+พอกันทีกับการ เสียเงิน เสียเวลา ไปกับการดูแลผิวและควบคุมน้ำหนักโดยเสียเงินไปอย่างสิ้นเปลือง ให้ nanoVA ช่วยดูแลคุณ แค่ดื่มวันละ 1 - 2 แก้ว ช่วยให้ระบบเผาผลาญดีขึ้น และ
+ มีคอลลาเจนที่ช่วยให้ผิวนุ่มลื่น ไม่ใช่แค่กาแฟลดน้ำหนักและผิวพรรณ แต่ยังช่วยคุมหิว เพื่อช่วยในการลดน้ำหนักด้วยไฟเบอร์ ไม่มีคอเลสเตอรอล ไม่มีไขมันทรานส์ มีสารสกัดนานๆชนิดรวม 18 อย่าง ปลอดภัยไร้สารเคมี
+        </div>
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/product1.webp"
+            alt="product"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/product2.webp"
+            alt="product2"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/product3.webp"
+            alt="product"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', marginBottom: '10px', textAlign: 'center' }}>
+  <div style={{ fontSize: '80px', fontFamily: 'Kanit', marginBottom: '20px', color: 'black' }}>
+  nano <span style={{ fontSize: '80px', color: '#710C04' }}>VA</span> <span style={{ fontSize: '80px', color: '#black' }}>cofee</span>
+  </div>
+</div>
+
+
+<div style={{ fontSize: '20px', fontFamily: 'Kanit', marginBottom: '10px', textAlign: 'center' }}>
+  <div style={{ fontSize: '50px', fontFamily: 'Kanit', marginBottom: '20px', color: 'black' }}>
+  กาแฟ นาโนวา <span style={{ fontSize: '30px', color: 'red' }}></span>
+  </div>
+</div>
+
+
+<div style={{ fontSize: '20px', fontFamily: 'Kanit', textAlign: 'left', marginBottom: '10px', marginLeft: '120px' ,marginRight: '120px' }}>
+
+<div className='font-family'>
+<p>กาแฟสำเร็จรูป 18-in-1</p>
+<p>หอม กลมกล่อม เข้มข้น เมล็ดกาแฟอาราบิก้าแท้จากประเทศมาเลเซีย</p>
+<p>1 ซองพลังงานแค่ 70 กิโลแคลอรี</p>
+<p>1 ซองพลังงานแค่ 70 กิโลแคลอรี</p>
+<p>0% คอเลสเตอรอล </p>
+<p>0% ไขมันทรานส์</p>
+<p>ผิวลื่น อ่อนวัยสุขภาพดีจากคอลลาเจน</p>
+<p>รสชาติกลมกล่อม</p>
+<p>มีไฟเบอร์สูงช่วยในการขับถ่าย</p>
+<p>เร่งระบบเผาผลาญไขมัน คุมหิว อิ่มนาน</p>
+<p>เลขที่จดแจ้ง 13-2-02660-2-0091</p>
+</div>
+
+</div>
+
+<div style={{ fontSize: '20px', fontFamily: 'Kanit', marginBottom: '10px', textAlign: 'center' }}>
+  <div style={{ fontSize: '150px', fontFamily: 'Kanit', marginBottom: '20px', color: 'black' }}>
+  รีวิว <span style={{ fontSize: '150px', color: 'red' }}></span>
+  </div>
+</div>
+<div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/review1.webp"
+            alt="review1"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+        
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/review2.webp"
+            alt="review2"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/review3.webp"
+            alt="review3"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', marginBottom: '10px', textAlign: 'center' }}>
+  <div style={{ fontSize: '150px', fontFamily: 'Kanit', marginBottom: '20px', color: 'black' }}>
+  มาตรฐานสินค้า <span style={{ fontSize: '150px', color: 'red' }}></span>
+  </div>
+</div>
+<div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/standards1.webp"
+            alt="standards1"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/standards2.webp"
+            alt="standards2"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/standards3.webp"
+            alt="standards3"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/standards4.webp"
+            alt="standards4"
+            style={{ maxWidth: '80%', height: 'auto' }}
+          />
+        </div>
+
+
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', marginBottom: '10px', textAlign: 'center' }}>
+  <div style={{ fontSize: '80px', fontFamily: 'Kanit', marginBottom: '20px', color: 'black' }}>
+  ผลิตในโรงงานที่ได้มาตรฐานสากล<span style={{ fontSize: '150px', color: 'red' }}></span>
+  </div>
+</div>
+
+<div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/factory1.webp"
+            alt="factory1"
+            style={{ maxWidth: '70%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/factory2.webp"
+            alt="factory2"
+            style={{ maxWidth: '70%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/factory3.webp"
+            alt="factory3"
+            style={{ maxWidth: '70%', height: 'auto' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '5px' }}>
+          <img
+            src="public/image/promotion.webp"
+            alt="promotion"
+            style={{ maxWidth: '70%', height: 'auto' }}
+          />
+        </div>
+        
+        <div style={{ fontSize: '20px', fontFamily: 'Kanit', marginBottom: '10px', textAlign: 'center' }}>
+  <div style={{ fontSize: '80px', fontFamily: 'Kanit', marginBottom: '20px', color: 'black' }}>
+  nano <span style={{ fontSize: '80px', color: '#710C04' }}>VA</span> 
+  </div>
+</div>
+
+</div>
+    </>
+  )
+}
