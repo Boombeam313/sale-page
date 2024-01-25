89ea5fc76f357b0714d9002e635e992ffc0f69ad
Anuwat <anuwat_bom@hotmail.co.th>
2024-01-25 16:05:42 +0700

    เสร็จแล้ว

--- src/components/form/Formname.jsx
+++ src/components/form/Formname.jsx
@@ -2,16 +2,20 @@
 
 import React, { useState, useRef } from "react";
 import styles from './Form.module.css';
-import { FaLine } from "react-icons/fa";
+import { FaLine, FaUpload, FaFile } from "react-icons/fa";
 import { IoCall } from "react-icons/io5";
 import { TiShoppingCart } from "react-icons/ti";
+import { FaTruckFast, FaRegCreditCard, FaCopy } from "react-icons/fa6";
+import { Radio } from 'antd';
 
 const Formname = () => {
   const [address, setAddress] = useState("");
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState(1);
+  const [selectedFile, setSelectedFile] = useState(null);
   const nameInputRef = useRef(null);
-  const [checked, setChecked] = useState(false);
+  const accountNumberRef = useRef(null);
 
   const handleOrderButtonClick = () => {
     alert("Order placed successfully!");
@@ -26,10 +30,80 @@ const Formname = () => {
 
   const handlePhoneChange = (event) => {
     const input = event.target.value;
-    // Check if the input is a non-negative number
-    if (-/^\d+$/.test(input) || input === "") {
+    if (/^\d+$/.test(input) || input === "") {
       setPhone(input);
     }
+  };
+
+  const handlePaymentMethodChange = (e) => {
+    setPaymentMethod(e.target.value);
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleCopyButtonClick = () => {
+    if (accountNumberRef.current) {
+      accountNumberRef.current.select();
+      document.execCommand('copy');
+    }
+  };
+
+  const renderBankAccountDetails = () => {
+    if (paymentMethod === 2) {
+      return (
+        <div className={styles.bankAccountDetails}>
+          <p>กรุณาโอนเงินไปที่บัญชีธนาคาร</p>
+          <table className={styles.bankAccountTable}>
+            <tbody>
+              <tr>
+                <td>ธนาคาร</td>
+                <td>กสิกรไทย</td>
+              </tr>
+              <tr>
+                <td>ชื่อบัญชี</td>
+                <td>บริษัท นาโนเวิร์ส จำกัด</td>
+              </tr>
+              <tr>
+                <td>เลขบัญชี</td>
+                <td className={styles.accountNumberRef}>
+                  <input
+                    type="text"
+                    readOnly
+                    value="133-3-48377-3"
+                    ref={accountNumberRef}
+                  />
+                  <button className={styles.copybutton} onClick={handleCopyButtonClick}><FaCopy /> คัดลอก</button>
+                </td>
+              </tr>
+              <tr>
+                <td>หลักฐานการโอนเงิน</td>
+                <td><div className={styles.uploadButton}>
+                <label htmlFor="fileInput" className={styles.uploadLabel}>
+                <FaUpload /> Upload File
+                </label>
+                <input
+                type="file"
+                id="fileInput"
+                accept=".jpg, .jpeg, .png, .gif"
+                onChange={handleFileChange}
+                style={{ display: "none" }}
+                />
+        {selectedFile && (
+          <span className={styles.selectedFile}>
+            <FaFile /> {selectedFile.name}
+          </span>
+        )}
+      </div></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -46,7 +120,7 @@ const Formname = () => {
       </div>
       <div className={styles.phoneinput}>
         <input
-          type="text" 
+          type="text"
           id="phone"
           placeholder="เบอร์โทร"
           value={phone}
@@ -61,37 +135,43 @@ const Formname = () => {
           onChange={(event) => setAddress(event.target.value)}
         />
       </div>
+
       <div className={styles.checkboxcollectngin}>
-      <input
-        type="checkbox"
-        id="checkbox"
-        checked={checked}
-        onChange={() => setChecked(!checked)}
-        />
-        <label className="checkboxlabel" htmlFor="checkbox">
-        เลือก
-        {checked && " (เลือกแล้ว)"}
-        </label>
+        <Radio.Group onChange={handlePaymentMethodChange} value={paymentMethod}>
+          <Radio value={1}>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เก็บเงินปลายทาง  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <FaTruckFast />
+          </Radio>
+        </Radio.Group>
       </div>
 
-      <div className={styles.buttonContainer}> 
-        <div className={styles.buttonGroup}> 
-          <a 
-            href="https://page.line.me/579vamkm?openQrModal=true" 
-            target="_blank" 
-            rel="noopener noreferrer" 
+      <div className={styles.checkboxcollectngin}>
+        <Radio.Group onChange={handlePaymentMethodChange} value={paymentMethod}>
+          <Radio value={2}>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;โอนเงิน   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <FaRegCreditCard />
+          </Radio>
+        </Radio.Group>
+      </div>
+
+      {renderBankAccountDetails()}
+
+      <div className={styles.buttonContainer}>
+        <div className={styles.buttonGroup}>
+          <a
+            href="https://page.line.me/579vamkm?openQrModal=true"
+            target="_blank"
+            rel="noopener noreferrer"
             className={styles.questionButton}
           >
-            <FaLine /> สอบถาม
+            <FaLine />สอบถาม
           </a>
           <button
-            className={styles.callButton} 
+            className={styles.callButton}
             onClick={handleCallButtonClick}
           >
             <IoCall /> โทร
           </button>
           <button
-            className={styles.orderButton} 
+            className={styles.orderButton}
             onClick={handleOrderButtonClick}
           >
             <TiShoppingCart /> สั่งซื้อ
