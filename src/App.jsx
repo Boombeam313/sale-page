e6769a4f58ee046bff3c9e684153ce7bfcdddd6e
Monthira19 <poo19052545@hotmail.com>
2024-01-26 10:03:31 +0700

    end

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Form from './components/form/Form';
-
+import Text from './components/form/text1/Text';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="form" element={<Form />} />
-        
+        <Route path="Text" element={<Text/>} />
       </Routes>
     </BrowserRouter>
   )
