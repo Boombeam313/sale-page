ccbf923d79f6dba8124943f9e6f0cadf740e12c8
Anuwat <anuwat_bom@hotmail.co.th>
2024-01-26 10:05:25 +0700

    up26

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
-import "bootstrap/dist/css/bootstrap.css"
-
-import './App.css'
-
+import "bootstrap/dist/css/bootstrap.css";
+import './App.css';
 import Formname from './components/form/Formname';
-
-
-
 
 function App() {
   return (
-    <div>
-    <Formname/>
-    
+    <div className="app">
+      <div className="left-panel"></div>
+      <div className="center-panel"><Formname /></div>
+      <div className="right-panel"></div>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
